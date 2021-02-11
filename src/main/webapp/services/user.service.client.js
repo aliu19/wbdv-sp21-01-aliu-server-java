5990b0c6ae291b640ee2f9a72a61e297c4f52b7b
aliu19 <liu.and@northeastern.edu>
2021-02-11 10:01:18 -0500

    Fixes the server and basis for controller

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -6,17 +6,18 @@ function AdminUserServiceClient() {
   this.updateUser = updateUser;
   this.url = 'https://wbdv-generic-server.herokuapp.com/api/001311078/users';
   var self = this;
+
   function createUser(user) {
     return fetch(self.url, {
       method: 'POST',
       headers: {
-        'content-type': 'applications/json'
+        'content-type': 'application/json'
       },
       body: JSON.stringify(user)
     })
-      .then(function (response) {
-        return response.json()
-      })
+    .then(function (response) {
+      return response.json()
+    })
   }
 
   function findAllUsers() {
@@ -31,14 +32,14 @@ function AdminUserServiceClient() {
   }
 
   function deleteUser(userId) { //promise
-    return fetch('${self.url}/{userId}', {method: 'DELETE'}) // not "get"
-      .then(function (response) {
-        return response.json()
-      })
+    return fetch(`${self.url}/${userId}`, {method: 'DELETE'}) // not "get"
+    .then(function (response) {
+      return response.json()
+    })
   }
-  
+
   function updateUser(userId, user) {
-    return fetch(`${self.url}/${userId}`, {
+    return fetch('${self.url}/${userId}', {
       method: 'PUT',
       body: JSON.stringify(user),
       headers: {
