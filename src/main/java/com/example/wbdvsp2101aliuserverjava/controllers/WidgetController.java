1393bc26cef9e4d618fc1f6c6e96e31a7fba67a7
aliu19 <liu.and@northeastern.edu>
2021-04-23 01:01:32 -0400

    Implemented findWidgetsForTopic and CORS

--- src/main/java/com/example/wbdvsp2101aliuserverjava/controllers/WidgetController.java
+++ src/main/java/com/example/wbdvsp2101aliuserverjava/controllers/WidgetController.java
@@ -4,10 +4,13 @@ import com.example.wbdvsp2101aliuserverjava.models.Widget;
 import com.example.wbdvsp2101aliuserverjava.services.WidgetService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class WidgetController {
 
   @Autowired
@@ -16,5 +19,12 @@ public class WidgetController {
   @GetMapping("/api/widgets")
   public List<Widget> findAllWidgets() {
     return service.findAllWidgets();
+  }
+
+  @GetMapping("/api/topics/{tid}/widgets")
+  public List<Widget> findWidgetsForTopic(
+      @PathVariable("tid") String topicId
+  ) {
+    return service.findWidgetsForTopic(topicId);
   }
 }
--- src/main/java/com/example/wbdvsp2101aliuserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvsp2101aliuserverjava/services/WidgetService.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Service;
 public class WidgetService {
   private List<Widget> widgets = new ArrayList<Widget>();
   {
-    Widget w1 = new Widget(123l, "ABC123", "HEADING", 1, "Widgets for Topic ABC123");
-    Widget w2 = new Widget(234l, "ABC123", "PARAGRAPH", 1, "Lorem Ipsum");
+    Widget w1 = new Widget(123l, "607670d5820c72001740e6c2", "HEADING", 1, "Widgets for Topic ABC123");
+    Widget w2 = new Widget(234l, "607670d5820c72001740e6c2", "PARAGRAPH", 1, "Lorem Ipsum");
     Widget w3 = new Widget(345l, "ABC234", "HEADING", 2, "Widgets for Topic ABC234");
     Widget w4 = new Widget(456l, "ABC234", "PARAGRAPH", 1, "Lorem Ipsum");
     Widget w5 = new Widget(567l, "ABC234", "PARAGRAPH", 1, "Lorem Ipsum");
@@ -24,5 +24,15 @@ public class WidgetService {
 
   public List<Widget> findAllWidgets() {
     return widgets;
+  }
+
+  public List<Widget> findWidgetsForTopic(String topicId) {
+    List<Widget> ws = new ArrayList<Widget>();
+    for(Widget w: widgets) {
+      if (w.getTopicId().equals(topicId)) {
+        ws.add(w);
+      }
+    }
+    return ws;
   }
 }
