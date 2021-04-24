c6bbadbb1b602873807947185b0feb92c8d4a87e
aliu19 <liu.and@northeastern.edu>
2021-04-24 16:24:57 -0400

    Added model params

--- src/main/java/com/example/wbdvsp2101aliuserverjava/models/Widget.java
+++ src/main/java/com/example/wbdvsp2101aliuserverjava/models/Widget.java
@@ -1,11 +1,37 @@
 package com.example.wbdvsp2101aliuserverjava.models;
 
 public class Widget {
+  private String name;
   private Long id;
   private String topicId;
   private String type;
   private Integer size;
   private String text;
+  private Integer widgetOrder;
+  private String src;
+  private Integer width;
+  private Integer height;
+  private String cssClass;
+  private String style;
+  private String value;
+
+  public Widget(String name, Long id, String topicId, String type, Integer size,
+      String text, Integer widgetOrder, String src, Integer width, Integer height,
+      String cssClass, String style, String value) {
+    this.name = name;
+    this.id = id;
+    this.topicId = topicId;
+    this.type = type;
+    this.size = size;
+    this.text = text;
+    this.widgetOrder = widgetOrder;
+    this.src = src;
+    this.width = width;
+    this.height = height;
+    this.cssClass = cssClass;
+    this.style = style;
+    this.value = value;
+  }
 
   public Widget(Long id, String topicId, String type, Integer size, String text) {
     this.id = id;
@@ -56,5 +82,69 @@ public class Widget {
 
   public void setSize(Integer size) {
     this.size = size;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getWidgetOrder() {
+    return widgetOrder;
+  }
+
+  public void setWidgetOrder(Integer widgetOrder) {
+    this.widgetOrder = widgetOrder;
+  }
+
+  public String getSrc() {
+    return src;
+  }
+
+  public void setSrc(String src) {
+    this.src = src;
+  }
+
+  public Integer getWidth() {
+    return width;
+  }
+
+  public void setWidth(Integer width) {
+    this.width = width;
+  }
+
+  public Integer getHeight() {
+    return height;
+  }
+
+  public void setHeight(Integer height) {
+    this.height = height;
+  }
+
+  public String getCssClass() {
+    return cssClass;
+  }
+
+  public void setCssClass(String cssClass) {
+    this.cssClass = cssClass;
+  }
+
+  public String getStyle() {
+    return style;
+  }
+
+  public void setStyle(String style) {
+    this.style = style;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
   }
 }
